42816306bd9e79062a2a5f3cd13b5a1cc183b302
Jasper Wong <wlcjasper@gmail.com>
2023-12-04 14:55:51 +0800

    the array of null values solution

--- list/index.js
+++ list/index.js
@@ -2,11 +2,35 @@
 
 const fs = require('fs');
 
-const allFiles =fs.readdir(process.cwd(), (err, filenames) =>{
-  if(err)
-  {
+const allFiles = fs.readdir(process.cwd(), (err, filenames) => {
+  if (err) {
     console.log(err);
-    return;  
+    return;
   }
-  console.log(filenames);
-});
+
+  const allStats = Array(filenames.length).fill(null);
+  console.log(allStats);
+
+  for (let name of filenames) {
+    const index = filenames.indexOf(name);
+    fs.lstat(name, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+      allStats[index] = stats;
+
+      const ready = allStats.every((element) => {
+        return element;
+      })
+      // console.log(name, stats.isFile())
+
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(filenames[index], stats.isFile())
+        })
+      }
+    })
+  }
+  console.log(allStats);
+  // this will return [null, null] because fs.lstat is an async function
+})
