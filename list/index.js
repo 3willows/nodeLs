5c378f5312f53af4b321a01f77cf446a4708583c
Jasper Wong <wlcjasper@gmail.com>
2023-12-04 15:59:22 +0800

    bug fix: method #1 now works

--- list/index.js
+++ list/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const util = require('util');
 
 // method #2
-// const lstat = util.promisify(fs.lstat);
+const lstat = util.promisify(fs.lstat);
 
 // method #3
 // const { lstat } = fs.promises;
@@ -15,28 +15,25 @@ fs.readdir(process.cwd(), async (err, filenames) => {
     return;
   }
 
-  //   for (let name of filenames){
-  //     try {
-  //     const stats = await lstat(name);
-  //     console.log(name, stats.isFile());
-  //     } catch (err){
-  //       console.log(err);
-  //     }
-  //   }
-  // });
+    for (let name of filenames){
+      try {
+      const stats = await lstat(name);
+      console.log(name, stats.isFile());
+      } catch (err){
+        console.log(err);
+      }
+    }
+  });
 
   // method #1
 
-  
-
-  const lstat = (filename) => {
-    return new Promise((resolve, reject) => {
-      fs.lstat(filename, (err, stats) => {
-        if (err) {
-          reject(err);
-        }
-      })
-      resolve(stats);
-    });
-  };
-})
+  // const lstat = (filename) => {
+  //   return new Promise((resolve, reject) => {
+  //     fs.lstat(filename, (err, stats) => {
+  //       if (err) {
+  //         reject(err);
+  //       }
+  //       resolve(stats);
+  //     })
+  //   });
+  // };
